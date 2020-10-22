a6848b15f470b2d17d29a9e370e332e133c67f01
Filip Rigoir <filip.rigoir@telenet.be>
2020-10-22 12:37:51 +0200

    Verbetering Raad-Het-Getal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 
 export class AppComponent { 
   
-  title = 'first-app';  
+  title = 'Raad het getal';  
 
   public randomNummer: number;
   public aantalPogingen: number; 
